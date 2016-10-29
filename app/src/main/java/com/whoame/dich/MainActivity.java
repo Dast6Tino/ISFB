474f1d9e18e231f2309cdd1d2e3d21d90a21307b
Alexandra Batanova <medwedizaa@mail.ru>
2016-10-29 23:08:17 +0300

    nechto

--- app/src/main/java/com/whoame/dich/MainActivity.java
+++ app/src/main/java/com/whoame/dich/MainActivity.java
@@ -3,33 +3,16 @@ package com.whoame.dich;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-<<<<<<< HEAD
 import android.support.v7.widget.RecyclerView;
 
 import com.whoame.dich.AllDopClasses.Categoryes;
+import com.whoame.dich.AllDopClasses.CryptographyActivity;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.provider.CalendarContract;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
 import android.view.View;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import java.security.Key;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-=======
-import android.view.View;
->>>>>>> a5a6ac2add19f6f25bec17a11ad010f979c3e083
 
 public class MainActivity extends AppCompatActivity {
 
@@ -45,18 +28,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-<<<<<<< HEAD
-
         /* Работа со списком - генерация сохраненных дат в норм список
         * */
     recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
 
     layoutManager = new LinearLayoutManager(this);
     recyclerView.setLayoutManager(layoutManager);
-=======
+
     public void onClick(View view) {
         Intent intent = new Intent(MainActivity.this, CryptographyActivity.class);
         startActivity(intent);
     }
->>>>>>> a5a6ac2add19f6f25bec17a11ad010f979c3e083
 }
