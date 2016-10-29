6f1e3fc598ad648ffbb737a5a4807a4707f9f9f4
Alexandra Batanova <medwedizaa@mail.ru>
2016-10-29 23:18:41 +0300

    fix conflict

--- app/src/main/java/com/whoame/dich/MainActivity.java
+++ app/src/main/java/com/whoame/dich/MainActivity.java
@@ -3,26 +3,14 @@ package com.whoame.dich;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-<<<<<<< HEAD
-=======
 import android.view.View;
->>>>>>> 7e4f926d1a145b7f2458213534534701b4aef1e0
 import android.support.v7.widget.RecyclerView;
 import com.whoame.dich.AllDopClasses.Categoryes;
-<<<<<<< HEAD
-import com.whoame.dich.AllDopClasses.CryptographyActivity;
-
 import java.util.ArrayList;
 import java.util.List;
 
-import android.view.View;
-
-=======
-import java.util.ArrayList;
-import java.util.List;
 
 //Todo посмотреть не устарело ли AppCompatActivity
->>>>>>> 7e4f926d1a145b7f2458213534534701b4aef1e0
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,7 +26,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-<<<<<<< HEAD
         /* Работа со списком - генерация сохраненных дат в норм список
         * */
     recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
@@ -46,15 +33,8 @@ public class MainActivity extends AppCompatActivity {
     layoutManager = new LinearLayoutManager(this);
     recyclerView.setLayoutManager(layoutManager);
 
-=======
->>>>>>> 7e4f926d1a145b7f2458213534534701b4aef1e0
     public void onClick(View view) {
         Intent intent = new Intent(MainActivity.this, AboutActivity.class);
         startActivity(intent);
     }
-<<<<<<< HEAD
-=======
-        /* Работа со списком - генерация сохраненных дат в норм список
-        * */
->>>>>>> 7e4f926d1a145b7f2458213534534701b4aef1e0
 }
