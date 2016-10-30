eced0072c0c784be324c561045fd8934c72ee4ad
Ivan Strashko <opr.v2n2@gmail.com>
2016-10-30 14:14:55 +0300

    New Tasks

---
+++ app/src/main/java/com/whoame/dich/MenuLinks/Tasks/Crypto2TasksActivity.java
@@ -0,0 +1,15 @@
+package com.whoame.dich.MenuLinks.Tasks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.whoame.dich.R;
+
+public class Crypto2TasksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crypto2_tasks);
+    }
+}
