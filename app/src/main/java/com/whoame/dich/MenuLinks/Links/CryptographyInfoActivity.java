2cae3548604623e4b78b0656947c86ec4fd0eee4
Ivan Strashko <opr.v2n2@gmail.com>
2016-10-30 12:32:04 +0300

    PamPam

--- app/src/main/java/com/whoame/dich/MenuLinks/Links/CryptographyInfoActivity.java
+++ app/src/main/java/com/whoame/dich/MenuLinks/Links/CryptographyInfoActivity.java
@@ -1,15 +1,15 @@
-package com.whoame.dich.MenuLinks.InfoLinks;
+package com.whoame.dich.MenuLinks.Links;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.whoame.dich.R;
 
-public class WebInfoActivity extends AppCompatActivity {
+public class CryptographyInfoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_web_info);
+        setContentView(R.layout.activity_cryptography_info);
     }
 }
